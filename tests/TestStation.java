dbaabb1856c9a32cb46244564b65df11ff90e6b5
Sandrikk <krevovasandra@gmail.com>
2023-09-07 13:49:36 +0200

    added csvs and TestStation.java

---
+++ tests/TestStation.java
@@ -0,0 +1,19 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestStation {
+    @Test
+    public void testConstructorStoreDataCorrectly() {
+        Station s = new Station(1, "a", 1, "a", "a", "a", "a", "a", "a", 1.2, 1.1);
+        assertNotNull(s);
+
+    }
+
+    @BeforeEach
+    public void createDeventerStation() {
+
+    }
+}
