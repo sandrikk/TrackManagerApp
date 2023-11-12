43db41100dacb635268a82d58e01b0ee78721bc2
Sandrikk <krevovasandra@gmail.com>
2023-11-12 11:16:11 +0100

    added drawing

--- src/assignment/Drawing.java
+++ src/assignment/Drawing.java
@@ -1,9 +1,6 @@
 package assignment;
 
-import model.Station;
-import model.Track;
 import utils.CsvReader;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.HashMap;
@@ -22,47 +19,39 @@ public class Drawing extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        // Calculate the minimum Y to move the map up
+        int minY = stations.values().stream().mapToInt(p -> p.y).min().orElse(0);
+        int yOffset = minY; // Move up by the minimum Y value
+
         // Draw stations
         for (Map.Entry<String, Point> stationEntry : stations.entrySet()) {
             Point point = stationEntry.getValue();
-            g.fillOval(point.x - 5, point.y - 5, 10, 10);
-            g.drawString(stationEntry.getKey(), point.x - 5, point.y - 5);
+            // Apply yOffset to move the map up
+            g.fillOval(point.x - 5, point.y - 5 - yOffset, 5, 5);
+            //g.drawString(stationEntry.getKey(), point.x - 5, point.y - 20 - yOffset); // Adjust y for text to prevent overlap with the dot
         }
+
+        // Set the color for the connections
+        g.setColor(Color.RED);
+
         // Draw connections
         for (Map.Entry<String, Map<String, Integer>> connectionEntry : connections.entrySet()) {
             String fromStation = connectionEntry.getKey();
-            for (String toStation : connectionEntry.getValue().keySet()) {
-                Point from = stations.get(fromStation);
-                Point to = stations.get(toStation);
-
-                // Only draw the line if both points are not null
-                if (from != null && to != null) {
-                    g.drawLine(from.x, from.y, to.x, to.y);
+            Point from = stations.get(fromStation);
+            if (from != null) {
+                for (String toStation : connectionEntry.getValue().keySet()) {
+                    Point to = stations.get(toStation);
+                    if (to != null) {
+                        // Apply yOffset to move the map up
+                        g.drawLine(from.x, from.y - yOffset, to.x, to.y - yOffset);
+                    }
                 }
             }
         }
-    }
 
-    private static double calculateScaleFactor(List<Point> allPoints, int canvasWidth, int canvasHeight) {
-        int minX = Integer.MAX_VALUE;
-        int maxX = Integer.MIN_VALUE;
-        int minY = Integer.MAX_VALUE;
-        int maxY = Integer.MIN_VALUE;
-
-        for (Point point : allPoints) {
-            if (point.x < minX) minX = point.x;
-            if (point.x > maxX) maxX = point.x;
-            if (point.y < minY) minY = point.y;
-            if (point.y > maxY) maxY = point.y;
-        }
-
-        double rangeX = maxX - minX;
-        double rangeY = maxY - minY;
-
-        double scaleX = canvasWidth / rangeX;
-        double scaleY = canvasHeight / rangeY;
-
-        return Math.min(scaleX, scaleY); // Use the smaller scale factor to fit everything on the canvas
+        // Reset the color for any further painting
+        g.setColor(Color.BLACK);
     }
 
     public static void main(String[] args) {
@@ -109,7 +98,7 @@ public class Drawing extends JPanel {
 
     private static Point convertGeoToScreen(double lat, double lng) {
         // You need a real implementation here
-        return new Point((int) (lng * 10), (int) (lat * 10));
+        return new Point((int) (lng * 40), (int) (lat * 40));
     }
 
 
